3c50bba0f0450b0e0bf5f0191a4cc8f7eac39468
Ihctimes <116114556+Ihctimes@users.noreply.github.com>
2023-06-19 16:14:42 +0300

    changed the folder name to something else

--- Post-Quake/Neumorphic/File-Assets/base-script.js
+++
@@ -1,9 +0,0 @@
-let sections = document.getElementsByClassName("section");
-
-
-function changeSection(index) {
-    for (i = 0;i < sections.length; i++) {
-        sections[i].classList.remove("selected-section");
-    }
-    sections[index-1].classList.add("selected-section");
-}
